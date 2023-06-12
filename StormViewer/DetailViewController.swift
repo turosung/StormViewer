0db43505f56b4696f093091726b8fcdc6f3faebf
Nuhu Sulemana <turosung@gmail.com>
2023-06-13 00:38:56 +0100

    completed challenge 3 for project 1

--- StormViewer/DetailViewController.swift
+++ StormViewer/DetailViewController.swift
@@ -11,10 +11,14 @@ class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
     var selectedImage: String?
     
+    var selectedPictureNumber = 0
+    
+    var totalPictures = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        title = selectedImage
+        title = "Picture \(selectedPictureNumber) of \(totalPictures)"
         navigationItem.largeTitleDisplayMode = .never
         
         if let imageToLoad = selectedImage {
